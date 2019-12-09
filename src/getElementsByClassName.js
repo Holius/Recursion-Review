0d8fea504f4ddee435e128e2383e12d5dd67144b
Hack Reactor Student <student@hackreactor.com>
2019-12-09 17:31:08 -0600

    getElementsByClassName init attempt

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,40 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var all = document.all;
+  var length = all.length;
+  console.log(arguments[1], arguments[2])
+
+  if (arguments[1] === undefined) {
+    arguments[1] = 0;
+  }
+
+  if (arguments[2] === undefined) {
+    arguments[2] = [];
+    }               
+  console.log(arguments[1], arguments[2])
+
+
+  if (arguments[1] === length - 1) {
+    for (let x = 0; x < all[arguments[1]].classList.length; x++) {
+        if (all[arguments[1]].classList[x] === className) {
+          arguments[2].push(all[arguments[1]]);  
+  
+      }
+      return arguments[2];
+    } 
+  }
+
+  else {
+    for (let x = 0; x < all[arguments[1]].classList.length; x++) {
+      if (all[arguments[1]].classList[x] === className) {
+        arguments[2].push(all[arguments[1]]);  
+      }
+    return getElementsByClassName(className, arguments[1] + 1, arguments[2])
+    } 
+  } 
+  // base case -- last element of ALL; 
+  // return RESULT somewhere   
 };
