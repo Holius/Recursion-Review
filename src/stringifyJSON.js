3dc79f5949d1754ae2617e6b895f2f69dfaa9b63
Hack Reactor Student <student@hackreactor.com>
2019-12-09 20:03:28 -0600

    stringifyJSON init version

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  if (typeof obj === 'function') {
+    return null
+  }  
+  if (typeof obj === 'boolean') {
+    return obj.toString()
+  }
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (obj === undefined) {
+    return 'undefined';
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var addMe = '['
+    for (var index of obj) {
+      var string = stringifyJSON(index);
+      addMe = addMe + string;
+    }
+    return addMe + ']'
+  }
+  if (typeof obj === 'object') {
+      var addMe = '{';
+        for (var keys in obj) {
+          var string = stringifyJSON(obj[keys]);
+          var key = keys.toString();
+          addMe = addMe + keys + ':' + string;                 
+      }
+      addMe = addMe + '}';
+      return addMe;
+  }
 };
+
